61cb5b9d04790c46dccc9087aaf5d6df712d19c9
Vicdejuan <dejuan.victor@gmail.com>
2014-10-21 11:40:52 +0200

    Añadida tabla hash

--- Ejercicios ASM/compare.asm
+++ Ejercicios ASM/compare.asm
@@ -1,19 +1,27 @@
 section .bss	
 	_x resd	1
+	_par resd 1
+	_impar resd 1
 
-section .data
-	_par db 2
-	_impar db 1
 section .text		
 	global main
 	extern scan_int, print_endofline, print_int
 
 main:
+
+
+	mov dword[_par],2
+	mov dword[_impar],1
 	push _x
 	call scan_int
 	add esp,4
+	mov ecx,2
 
-
+	mov eax,2
+	mov edx,0
+	div dword[_x]
+	test edx,0
+	jnz impar
 		
 	push dword [_par]
 	call print_int
