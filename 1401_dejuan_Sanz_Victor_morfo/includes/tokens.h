d16db0f386ef0a2b5840c4117c5a5640db82df81
Vicdejuan <dejuan.victor@gmail.com>
2014-10-29 01:51:48 +0100

    Terminado. A falta de estilo mejorable

--- 1401_dejuan_Sanz_Victor_morfo/includes/tokens.h
+++ 1401_dejuan_Sanz_Victor_morfo/includes/tokens.h
@@ -54,49 +54,60 @@ Descripción: Definición de tokens para el lenguaje ALFA
 #define TOK_TRUE                401
 #define TOK_FALSE               402
 
+
+#define TOK_JUMP               500
+#define TOK_LINE_UP               501
+#define TOK_COL_UP             502
+
 /* Errores */
 
-#define TOK_ERROR 1
+#define TOK_ERROR -1
+#define TOK_ERROR_LONG -2
+
+
+
 /* Mensajes */
 
-#define TOK_MAIN_STR "TOK_MAIN %d %s"
-#define TOK_INT_STR "TOK_INT %d %s"
-#define TOK_BOOLEAN_STR "TOK_BOOLEAN %d %s"
-#define TOK_ARRAY_STR "TOK_ARRAY %d %s"
-#define TOK_FUNCTION_STR "TOK_FUNCTION %d %s"
-#define TOK_IF_STR "TOK_IF %d %s"
-#define TOK_ELSE_STR "TOK_ELSE %d %s"
-#define TOK_WHILE_STR "TOK_WHILE %d %s"
-#define TOK_SCANF_STR "TOK_SCANF %d %s"
-#define TOK_PRINTF_STR "TOK_PRINTF %d %s"
-#define TOK_RETURN_STR "TOK_RETURN %d %s"
-#define TOK_PUNTOYCOMA_STR "TOK_PUNTOYCOMA %d %s"
-#define TOK_COMA_STR "TOK_COMA %d %s"
-#define TOK_PARENTESISIZQUIERDO_STR "TOK_PARENTESISIZQUIERDO %d %s"
-#define TOK_PARENTESISDERECHO_STR "TOK_PARENTESISDERECHO %d %s"
-#define TOK_CORCHETEIZQUIERDO_STR "TOK_CORCHETEIZQUIERDO %d %s"
-#define TOK_CORCHETEDERECHO_STR "TOK_CORCHETEDERECHO %d %s"
-#define TOK_LLAVEIZQUIERDA_STR "TOK_LLAVEIZQUIERDA %d %s"
-#define TOK_LLAVEDERECHA_STR "TOK_LLAVEDERECHA %d %s"
-#define TOK_ASIGNACION_STR "TOK_ASIGNACION %d %s"
-#define TOK_MAS_STR "TOK_MAS %d %s"
-#define TOK_MENOS_STR "TOK_MENOS %d %s"
-#define TOK_DIVISION_STR "TOK_DIVISION %d %s"
-#define TOK_ASTERISCO_STR "TOK_ASTERISCO %d %s"
-#define TOK_AND_STR "TOK_AND %d %s"
-#define TOK_OR_STR "TOK_OR %d %s"
-#define TOK_NOT_STR "TOK_NOT %d %s"
-#define TOK_IGUAL_STR "TOK_IGUAL %d %s"
-#define TOK_DISTINTO_STR "TOK_DISTINTO %d %s"
-#define TOK_MENORIGUAL_STR "TOK_MENORIGUAL %d %s"
-#define TOK_MAYORIGUAL_STR "TOK_MAYORIGUAL %d %s"
-#define TOK_MENOR_STR "TOK_MENOR %d %s"
-#define TOK_MAYOR_STR "TOK_MAYOR %d %s"
-#define TOK_IDENTIFICADOR_STR "TOK_IDENTIFICADOR %d %s"
-#define TOK_CONSTANTE_ENTERA_STR "TOK_CONSTANTE_ENTERA %d %s"
-#define TOK_TRUE_STR "TOK_TRUE %d %s"
-#define TOK_FALSE_STR "TOK_FALSE %d %s"
-#define TOK_ERROR_STR "TOK_ERROR %d %s"
+
+
+#define TOK_MAIN_STR "TOK_MAIN %d %s\n"
+#define TOK_INT_STR "TOK_INT %d %s\n"
+#define TOK_BOOLEAN_STR "TOK_BOOLEAN %d %s\n"
+#define TOK_ARRAY_STR "TOK_ARRAY %d %s\n"
+#define TOK_FUNCTION_STR "TOK_FUNCTION %d %s\n"
+#define TOK_IF_STR "TOK_IF %d %s\n"
+#define TOK_ELSE_STR "TOK_ELSE %d %s\n"
+#define TOK_WHILE_STR "TOK_WHILE %d %s\n"
+#define TOK_SCANF_STR "TOK_SCANF %d %s\n"
+#define TOK_PRINTF_STR "TOK_PRINTF %d %s\n"
+#define TOK_RETURN_STR "TOK_RETURN %d %s\n"
+#define TOK_PUNTOYCOMA_STR "TOK_PUNTOYCOMA %d %s\n"
+#define TOK_COMA_STR "TOK_COMA %d %s\n"
+#define TOK_PARENTESISIZQUIERDO_STR "TOK_PARENTESISIZQUIERDO %d %s\n"
+#define TOK_PARENTESISDERECHO_STR "TOK_PARENTESISDERECHO %d %s\n"
+#define TOK_CORCHETEIZQUIERDO_STR "TOK_CORCHETEIZQUIERDO %d %s\n"
+#define TOK_CORCHETEDERECHO_STR "TOK_CORCHETEDERECHO %d %s\n"
+#define TOK_LLAVEIZQUIERDA_STR "TOK_LLAVEIZQUIERDA %d %s\n"
+#define TOK_LLAVEDERECHA_STR "TOK_LLAVEDERECHA %d %s\n"
+#define TOK_ASIGNACION_STR "TOK_ASIGNACION %d %s\n"
+#define TOK_MAS_STR "TOK_MAS %d %s\n"
+#define TOK_MENOS_STR "TOK_MENOS %d %s\n"
+#define TOK_DIVISION_STR "TOK_DIVISION %d %s\n"
+#define TOK_ASTERISCO_STR "TOK_ASTERISCO %d %s\n"
+#define TOK_AND_STR "TOK_AND %d %s\n"
+#define TOK_OR_STR "TOK_OR %d %s\n"
+#define TOK_NOT_STR "TOK_NOT %d %s\n"
+#define TOK_IGUAL_STR "TOK_IGUAL %d %s\n"
+#define TOK_DISTINTO_STR "TOK_DISTINTO %d %s\n"
+#define TOK_MENORIGUAL_STR "TOK_MENORIGUAL %d %s\n"
+#define TOK_MAYORIGUAL_STR "TOK_MAYORIGUAL %d %s\n"
+#define TOK_MENOR_STR "TOK_MENOR %d %s\n"
+#define TOK_MAYOR_STR "TOK_MAYOR %d %s\n"
+#define TOK_IDENTIFICADOR_STR "TOK_IDENTIFICADOR %d %s\n"
+#define TOK_CONSTANTE_ENTERA_STR "TOK_CONSTANTE_ENTERA %d %s\n"
+#define TOK_TRUE_STR "TOK_TRUE %d %s\n"
+#define TOK_FALSE_STR "TOK_FALSE %d %s\n"
+#define TOK_ERROR_STR "TOK_ERROR %d %s\n"
 
 
 #endif
