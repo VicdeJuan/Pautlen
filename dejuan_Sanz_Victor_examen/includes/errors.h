0e21a98b2f83c54c47d2587e3bdf6d49600c2566
VicdeJuan <dejuan.victor@gmail.com>
2015-01-23 13:39:01 +0100

    Añadido mi código del examen final

---
+++ dejuan_Sanz_Victor_examen/includes/errors.h
@@ -0,0 +1,16 @@
+#ifndef ERRORS_H
+#define ERRORS_H 
+
+#define OK 0
+
+#define ERR -1
+#define ERR_REPEAT -2
+#define ERR_NOTFOUND -4
+#define ERR_PARSE -8
+#define ERR_IO -16
+#define ERR_MEM -64
+#define ERR_RANGE -128
+#define ERR_SYS -256
+#define ERR_GLOBALLY -512
+#define ERR_NOTTYPE -1024
+#endif
